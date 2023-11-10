453c720a00d37714b1115c3671e22a46e1d90c02
topisteronyango <t.onyango@alustudent.com>
2023-11-10 21:08:31 +0200

    predict function

--- model_utils.py
+++ model_utils.py
@@ -89,19 +89,7 @@ def train_model(model, train_loader, valid_loader, learning_rate=0.01, epochs=5,
 
         print(f'Validation Loss: {validation_loss/len(valid_loader)}, Accuracy: {accuracy/len(valid_loader)}')
 
-# def save_checkpoint(model, class_to_idx, arch, hidden_units, save_dir='checkpoint'):
-#     checkpoint = {
-#         'arch': arch,
-#         'hidden_units': hidden_units,
-#         'state_dict': model.state_dict(),
-#         'class_to_idx': class_to_idx
-#     }
-    
-#     os.makedirs(save_dir, exist_ok=True)
 
-
-# #     torch.save(checkpoint, f'{save_dir}/checkpoint.pth')
-#     torch.save(checkpoint, os.path.join(save_dir, 'checkpoint.pth'))
 
 def save_checkpoint(model, class_to_idx, arch, hidden_units):
     os.makedirs(save_dir, exist_ok=True)
@@ -118,35 +106,48 @@ def save_checkpoint(model, class_to_idx, arch, hidden_units):
     torch.save(checkpoint, 'checkpoint.pth')
 
 
-
 def load_checkpoint(filepath):
-    # Load the checkpoint from the specified file path
-    checkpoint = torch.load("checkpoint.pth")
-
-    # Example: Load model architecture, state_dict, and other information
-    model = checkpoint['model']
+    checkpoint = torch.load(filepath, map_location=torch.device('cpu'))
+    
+    if checkpoint['arch'] == 'vgg16':
+        model = models.vgg16(pretrained=True)
+    elif checkpoint['arch'] == 'densenet121':
+        model = models.densenet121(pretrained=True)
+    else:
+        raise ValueError("Architecture not supported. Please choose 'vgg16' or 'densenet121'")
+    
+    # Freeze parameters so we don't backprop through them
+    for param in model.parameters():
+        param.requires_grad = False
+    
+    # Load the model state dict
     model.load_state_dict(checkpoint['state_dict'])
     model.class_to_idx = checkpoint['class_to_idx']
     
-    return model
-
+    return model, model.class_to_idx
 
 def predict(image_path, model, topk=5):
+    # Process the image
+    processed_image = process_image(image_path)
+    processed_image = torch.from_numpy(processed_image).float().unsqueeze(0)
+    
+    # Set the model to evaluation mode
     model.eval()
     
+    # Make the prediction
     with torch.no_grad():
-        image = process_image(image_path)
-        image = torch.from_numpy(image).float().unsqueeze(0)
-        
-        if gpu:
-            model.cuda()
-            image = image.cuda()
-        
-        output = model(image)
-        
-        probabilities, classes = torch.topk(output, topk)
-        probabilities = torch.nn.functional.softmax(probabilities[0], dim=0).numpy()
-        classes = [model.class_to_idx[idx] for idx in classes[0].cpu().numpy()]
-        
-    return probabilities, classes
+        output = model(processed_image)
+    
+    # Get the top probabilities and classes
+    probabilities, classes = torch.topk(output, topk)
+    
+    # Convert indices to class labels
+    idx_to_class = {idx: class_label for class_label, idx in model.class_to_idx.items()}
+    top_classes = [idx_to_class[idx.item()] for idx in classes[0]]
+    
+    # Convert probabilities to numpy array
+    probabilities = probabilities.exp().numpy()
+    
+    return probabilities[0], top_classes
+
 
